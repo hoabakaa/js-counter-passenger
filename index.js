ea13b298155aed45344a2ebba7d43515abcaa5a7
harusora <longcnhe141293@gmail.com>
2022-07-19 23:07:47 +0700

    done

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ let countEl = document.getElementById("count-el");
 
 let count = 0;
 
+let saveEl = document.getElementById("save-el");
+
 function increment() {
-    count = count + 1;
-    countEl.innerHTML = count;
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
